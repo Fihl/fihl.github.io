e30965df55cd9b9e0b6b53654613b15e3037f5b6
Fihl <OZ1AAB@Fihl.net>
2024-10-23 18:32:37 +0200

    xx

--- BLE3/src/demo/index.js
+++ BLE3/src/demo/index.js
@@ -4,7 +4,7 @@ import "./css/index.css";
 /**
  * Demos
  */
-//import BasicDemo from "./BasicDemo";
+import BasicDemo from "./BasicDemo";
 //import RTLDemo from "./RTLDemo";
 //import ButtonThemeDemo from "./ButtonThemeDemo";
 //import DOMElementDemo from "./DOMElementDemo";
