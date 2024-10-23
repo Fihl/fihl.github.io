bcaa1193b985e49921cda915cc018f9dab82b48f
Fihl <OZ1AAB@Fihl.net>
2024-10-23 20:03:06 +0200

    xx

--- BLE3/src/demo/index.js
+++ BLE3/src/demo/index.js
@@ -17,7 +17,17 @@ import CandidateBoxDemo from "./CandidateBoxDemo";
  */
 const SELECTED_DEMO = CandidateBoxDemo;
 
-/**
- * Bootstrap
- */
-new SELECTED_DEMO();
+
+const keyboard = new Keyboard({
+  onChange: input => onChange(input),
+  onKeyPress: button => onKeyPress(button)
+});
+
+function onChange(input){
+  document.querySelector(".input").value = input;
+  console.log("Input changed", input);
+}
+
+function onKeyPress(button){
+  console.log("Button pressed", button);
+}
